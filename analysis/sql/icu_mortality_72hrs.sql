08fc7af3aab0567d09ef44287d7d8466bdeb2fdc
claudia1501 <claujv164@gmail.com>
2023-06-26 10:59:01 +0200

    icu_mortality_72h

--- analysis/sql/icu_mortality_72hrs.sql
+++ analysis/sql/icu_mortality_72hrs.sql
@@ -2,7 +2,7 @@
 
 SELECT
   patientunitstayid,
-  MAX(CASE WHEN actualICULOS BETWEEN 1 AND 3 AND actualICUMortality = '1' THEN 1
+  MAX(CASE WHEN actualICULOS BETWEEN 1 AND 3 AND actualICUMortality = 'EXPIRED' THEN 1
     ELSE 0 END) AS icu_mortality_72hrs
 FROM
   `physionet-data.eicu_crd.apachepatientresult`
