915fe1e193da64e496e930252422cfb2d2231c82
claudia1501 <claujv164@gmail.com>
2023-10-27 10:44:56 +0200

    CHANGE DF ICU MORTALITY. DELETE APACHESCORE

--- analysis/sql/icu_mortality_72hrs.sql
+++ analysis/sql/icu_mortality_72hrs.sql
@@ -1,10 +1,10 @@
 -- Calculates ICU mortality between 24 and 72hrs and add apachescore variable (Calculated from acutePhysiologyScore)
 
 SELECT
-  patientunitstayid, apachescore,
+  patientunitstayid,
   MAX(CASE WHEN actualICULOS BETWEEN 1 AND 3 AND actualICUMortality = 'EXPIRED' THEN 1
     ELSE 0 END) AS icu_mortality_72hrs
 FROM
   `physionet-data.eicu_crd.apachepatientresult`
 GROUP BY
-  patientunitstayid, apachescore
+  patientunitstayid
